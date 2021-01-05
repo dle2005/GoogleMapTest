2dae78abd5f4f144f41dcb8b3f28d88c34808769
Dongkon Lee <dle0129@gmail.com>
2021-01-05 10:36:56 +0900

    First Commit

---
+++ app/src/main/java/com/example/googlemaptest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.googlemaptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
